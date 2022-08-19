e1b6bac6a9043b1e638452ca8997cc35a68bdd94
Sebastián Fuenzalida Garrido <sebastian.fuenzalida.garrido@gmail.com>
2022-08-19 18:06:16 -0400

    New modifications in the sTGC plotting tool. However, but the plots corresponding to the ASide for some reason are not being stored...

--- OrganizerScript/organizer.sh
+++ OrganizerScript/organizer.sh
@@ -1,46 +1,104 @@
 #!/usr/bin/bash
 
-base_path=/home/sebastian/sTGC_output_plots/plots
-input_path=/home/sebastian/sTGC_output_plots/sTGC_plotting_tool/InputHistograms
-script_path=/home/sebastian/sTGC_output_plots/OrganizerScript
-python_script_path=/home/sebastian/sTGC_output_plots/sTGC_plotting_tool/SourceCode
+base_path=/home/Sebastian/sTGC_output_plots
+
+output_plots_path=/home/Sebastian/sTGC_output_plots/plots
+input_ROOT_files_path=/home/Sebastian/sTGC_output_plots/sTGC_plotting_tool/InputHistograms
+organizer_bash_script_path=/home/Sebastian/sTGC_output_plots/OrganizerScript
+python_sTGC_plotting_tool_path=/home/Sebastian/sTGC_output_plots/sTGC_plotting_tool/SourceCode
 
 sub_folders=("Overview" "Summary")
 sub_sub_folders=("ASide" "CSide")
 run_number=()
 file_names=()
 
-cd $base_path
+echo "What operation do you want to execute?"
+echo "Create skeleton            -> 0"
+echo "Reorganize inputs          -> 1"
+echo "Execute sTGC plotting tool -> 2"
+read answer
 
-for i in $(ls $input_path); do
-    #echo "In the path, "$base_path" we are creating the folders: "$i
-    run_number+=($(echo $i | cut -b 18-23))
-    file_names+=($i)
-    for j in ${sub_folders[@]}; do
-
-	mkdir -p -v $i/$j
-	
-	if [[ $j == "Summary" ]]; then
-	    for k in ${sub_sub_folders[@]}; do
-		mkdir -p -v $i/$j/$k
-	    done
+if (( $answer == 0 )); then
+    echo "Creating skeleton..."
+    for i in $(ls $input_ROOT_files_path); do
+	if [[ $i == "inputsOrganized" ]]; then
+	    continue
 	fi
-    done  
-done
+	
+	for j in ${sub_folders[@]}; do
+	    if [[ $j == "Overview" ]]; then
+		mkdir -p -v $output_plots_path/$i/$j
+	    else
+		for k in ${sub_sub_folders[@]}; do
+		    mkdir -p -v $output_plots_path/$i/$j/$k
+		done
+	    fi
+	done  
+    done
+    echo "Skeleton created!"
+elif (( $answer == 1 )); then
+    echo "Organizing input histogram ROOT files..."
+    mkdir -p -v $input_ROOT_files_path/inputsOrganized
+    
+    for i in $(ls $input_ROOT_files_path); do
+	if [[ $i == "inputsOrganized" ]]; then
+	    continue
+	fi
 
-
-cd $python_script_path
-
-for i in ${!run_number[@]}; do
-    for j in ${!sub_folders[@]}; do
-	for k in ${!sub_sub_folders[@]}; do
-	    python3 plotting.py --runNumber ${run_number[$i]} --inputROOTFile ${file_names[$i]} --side ${!sub_folders[j]} --folder ${!sub_sub_folders[k]}
+	for j in $(ls $input_ROOT_files_path/$i); do
+	    cp -v $input_ROOT_files_path/$i/$j $input_ROOT_files_path/inputsOrganized 
 	done
     done
-done
+    
+    echo "Files organized!"
+elif (( $answer == 2 )); then
+    echo "Executing python sTGC plotting tool..."
+    
+    for i in $(ls $input_ROOT_files_path/inputsOrganized); do
+	if [[ $i == "inputsOrganized" ]]; then
+	    continue
+	fi
+	
+	run_number+=($(echo $i | cut -b 18-23))
+	file_names+=($(echo $i | cut -b 1-58))
+    done
+
+    cd $python_sTGC_plotting_tool_path
+    
+    for i in ${!file_names[@]}; do
+	for j in ${!sub_folders[@]}; do
+	    if [[ ${sub_folders[j]} == "Overview" ]]; then
+		echo "file: "${file_names[i]}", run: "${run_number[i]}", folder: "${sub_folders[j]}
+		python plotting.py --runNumber ${run_number[i]} --inputROOTFile ${file_names[i]} --folder ${sub_folders[j]}  
+	    else
+		for k in ${!sub_sub_folders[@]}; do
+		    echo "file: "${file_names[i]}", run: "${run_number[i]}", folder: "${sub_folders[j]}", side: "${sub_sub_folders[k]}
+		    python plotting.py --runNumber ${run_number[i]} --inputROOTFile ${file_names[i]} --side ${sub_sub_folders[j]} --folder ${sub_folders[k]}
+		done
+	    fi
+	done
+    done
+    
+    echo "Execution finalized!"
+else
+    echo "Invalid option entered :( Try again!"
+    return
+fi
+
+cd $base_path
+
+#cd $python_sTGC_plotting_tool_path
+
+#for i in ${!run_number[@]}; do
+#    for j in ${!sub_folders[@]}; do
+#	for k in ${!sub_sub_folders[@]}; do
+#	    python3 plotting.py --runNumber ${run_number[$i]} --inputROOTFile ${file_names[$i]} --side ${!sub_folders[j]} --folder ${!sub_sub_folders[k]}
+#	done
+#    done
+#done
 
 
-#cd $script_path
+#cd $organizer_bash_script_path
 
 #mkdir
 #cd
--- sTGC_plotting_tool/SourceCode/plotting.py
+++ sTGC_plotting_tool/SourceCode/plotting.py
@@ -7,24 +7,23 @@ R.gROOT.SetBatch(True)
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--runNumber", default = "run_410000", help = "Enter the name of the run.")
 parser.add_argument("--inputROOTFile", default = "monitor_sTgc.root", help = "Name of the input samples with histograms and directories")
-parser.add_argument("--side", default = "ASide", help = "Select the side over which you want to extract the plots (ASide or CSide).")
+parser.add_argument("--runNumber", default = "run_410000", help = "Enter the name of the run.")
 parser.add_argument("--folder", default = "Overview", help = "Select the directory over which you want to extract the plots (Overview, Summary).")
+parser.add_argument("--side", default = None, help = "Select the side over which you want to extract the plots (ASide or CSide).")
 args = parser.parse_args()
 
 
 ROOT_file_base_dir = None
 
 if (args.folder == "Overview"):
-    ROOT_file_base_dir   = "run_" + args.runNumber + "/Muon/MuonRawDataMonitoring/sTgc/Overview" 
-    os.chdir("/home/sebastian/sTGC_output_plots/plots/" + args.inputROOTFile + "/" + args.folder)
-else:
+    ROOT_file_base_dir   = "run_" + args.runNumber + "/Muon/MuonRawDataMonitoring/sTgc/Overview"
+    os.chdir("/home/Sebastian/sTGC_output_plots/plots/" + args.inputROOTFile + "/" + args.folder)
+elif (args.folder == "Summary"):
     ROOT_file_base_dir   = "run_" + args.runNumber + "/Muon/MuonRawDataMonitoring/sTgc/" + args.side  + "/" + args.folder
-    os.chdir("/home/sebastian/sTGC_output_plots/plots/" + args.inputROOTFile + "/" + args.folder + "/" + args.side)
+    os.chdir("/home/Sebastian/sTGC_output_plots/plots/" + args.inputROOTFile + "/" + args.folder + "/" + args.side)
     
-input_ROOT_file_path = args.inputROOTFile + "._0001.1"
-
+input_ROOT_file_path = "/home/Sebastian/sTGC_output_plots/sTGC_plotting_tool/InputHistograms/inputsOrganized/" + args.inputROOTFile + "._0001.1"
 
 ROOT_file = R.TFile.Open(input_ROOT_file_path, "read")
 get_keys_base_dir = ROOT_file.Get(ROOT_file_base_dir)
